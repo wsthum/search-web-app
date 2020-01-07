42e0e887d6053b5fc4f5ff1b08a7ca46e5db7766
unknown <thum.weisern@stratussilverlining.com>
2020-01-07 11:30:46 -0600

    Fixed tab vs space issues in config file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	apiPath: "/api",
-	documentQueryApiPath: "./server/routes/documentQuery",
-	findKeysApiPath: "./server/routes/getKeys",
-	dataFolderPath: __dirname + "/../server/data/",
-	mockDataFolderPath: __dirname + "/../test/mockData/",
-	serverPort: 3002
+  apiPath: "/api",
+  documentQueryApiPath: "./server/routes/documentQuery",
+  findKeysApiPath: "./server/routes/getKeys",
+  dataFolderPath: __dirname + "/../server/data/",
+  mockDataFolderPath: __dirname + "/../test/mockData/",
+  serverPort: 3002
 }
