44b7e33344a5cf5e192e0588d020660cae7d23b7
Wei Sern Thum <thum.weisern@stratussilverlining.com>
2020-01-07 02:11:05 -0600

    comment change

--- server/routes/getKeys.js
+++ server/routes/getKeys.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var getKeysCtrl = require("../controllers/getKeys");
 
-// Contains API route names for query file request
+// Contains API route names for get key request
 router.get("/keys", getKeysCtrl.getKeys);
 
 module.exports = router;
